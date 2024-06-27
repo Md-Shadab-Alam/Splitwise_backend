4d953323f66929fbfed70f6ed2ca9bc08c2d56db
Md-Shadab-Alam <shadabalam.1725@gmail.com>
2024-06-27 13:06:34 +0530

    Auth 2

---
+++ Controllers/AuthTestController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Splitwise.Utility;
+
+namespace Splitwise.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthTestController : ControllerBase
+    {
+        [HttpGet]
+        [Authorize]
+        public async Task<ActionResult<string>> GetSomething()
+        {
+            return "You are authenticated";
+        }
+
+        [HttpGet("{id:int}")]
+        [Authorize(Roles = StorageData.Role_Admin)]
+        public async Task<ActionResult<string>> GetSomething(int someIntValue)
+        {
+            return "You are Authorized with this role";
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,8 +1,14 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using Splitwise.Data;
 using Splitwise.Entities;
 using Splitwise.Services;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Cryptography.Xml;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,7 +18,38 @@ builder.Services.AddControllers();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition(JwtBearerDefaults.AuthenticationScheme, new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+    {
+        Description=
+            "JWT Authorization header using the Bearer scheme. \r\n\r\n"+
+            "Enter 'Bearer' [space and then your token in the text input below. \r\n\r\n" +
+            "Example: \"Bearer 12345abcdef\"",
+        Name="Authorization",
+        In=Microsoft.OpenApi.Models.ParameterLocation.Header,
+        Scheme = JwtBearerDefaults.AuthenticationScheme 
+    });
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement()
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    },
+                Scheme = "oauth2",
+                Name = "Bearer",
+                In = ParameterLocation.Header
+            },
+            new List<string>()
+        }
+});
+});
+
 builder.Services.AddDbContext<SplitwiseDbContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("SplitwiseConnectionString")));
 builder.Services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<SplitwiseDbContext>();
@@ -26,6 +63,23 @@ builder.Services.Configure<IdentityOptions>(options =>
     options.Password.RequireNonAlphanumeric = false;
 });
 
+var key = builder.Configuration.GetValue<string>("ApiSettings:Secret");
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(u =>
+{
+    u.RequireHttpsMetadata = false;
+    u.SaveToken = true;
+    u.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(key)),
+        ValidateIssuer = false,
+        ValidateAudience = false,
+    };
+});
 
 
 //Adding Services
